3806feb282eaf4230ba06cb6eb7eb6daf7b2e15b
KRENOWICZ <nkrenowicz@cdaid.org>
2022-11-21 16:53:50 -0800

    beginning normalization

--- gestures.py
+++ gestures.py
@@ -1,8 +1,8 @@
+import copy
 import numpy as np
 import cv2
 import mediapipe as mp
 import time
-
 
 
 class GESTURES():
@@ -48,6 +48,7 @@ class GESTURES():
             for hand_landmarks in results.multi_hand_landmarks:
                 print("----------------------------")
                 print(hand_landmarks)
+                self.normalize_landmarks(image, hand_landmarks)
                 self.mp_drawing.draw_landmarks(
                     image,
                     hand_landmarks,
@@ -56,6 +57,37 @@ class GESTURES():
                     self.mp_drawing_styles.get_default_hand_connections_style())
         
         return image
+
+    # https://github.com/kinivi/hand-gesture-recognition-mediapipe/blob/0e737bb8c45ea03f6fafb1f5dbfe9246c34a8003/app.py#L231
+    def normalize_landmarks(self, image, landmarks):
+            temp_landmark_list = copy.deepcopy(landmarks)
+
+            # Convert to relative coordinates
+            base_x, base_y = 0, 0
+            for index, landmark_point in enumerate(temp_landmark_list):
+                if index == 0:
+                    base_x, base_y = landmark_point[0], landmark_point[1]
+
+                temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
+                temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+            
+            print("------------")
+            print(temp_landmark_list)
+            # Convert to a one-dimensional list
+            temp_landmark_list = list(
+                itertools.chain.from_iterable(temp_landmark_list))
+
+            # Normalization
+            max_value = max(list(map(abs, temp_landmark_list)))
+
+            def normalize_(n):
+                return n / max_value
+
+            temp_landmark_list = list(map(normalize_, temp_landmark_list))
+
+            return temp_landmark_list
+        
+        
 
 
     def disploy_output(self, image):
